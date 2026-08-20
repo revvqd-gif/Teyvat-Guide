d8ceef30edb58e8c6bf042e1e112059eb0c6619a
Builder <builder@local>
2026-08-20 09:27:51 +0700

    Fix KSP: add Converters import

--- app/src/main/java/com/teyvatmap/data/db/TeyvatMapDatabase.kt
+++ app/src/main/java/com/teyvatmap/data/db/TeyvatMapDatabase.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.teyvatmap.data.db.MapEntities.*
+import com.teyvatmap.data.db.Converters
 
 @Database(
     entities = [
